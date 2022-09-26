11a96c6cc0b38162b36df30222aec5fa6cc3b496
barath0211 <barathkumaarabirami@gmail.com>
2022-09-26 10:45:17 +0530

    Starting the Pet clinic app

---
+++ src/main/java/barath/springframework/sfgpetclinic/PetCLinicApplication.java
@@ -0,0 +1,13 @@
+package barath.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCLinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCLinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/barath/springframework/sfgpetclinic/PetCLinicApplicationTests.java
@@ -0,0 +1,13 @@
+package barath.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCLinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
