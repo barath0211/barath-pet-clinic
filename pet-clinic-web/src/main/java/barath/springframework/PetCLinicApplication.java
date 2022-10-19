067a4c3e4725c3cf9d52f2c8399c8bdc7a7cd90f
barath0211 <barathkumaarabirami@gmail.com>
2022-10-19 11:03:23 +0530

    Rectifying errors

--- pet-clinic-web/src/main/java/barath/springframework/PetCLinicApplication.java
+++ pet-clinic-web/src/main/java/barath/springframework/PetCLinicApplication.java
@@ -1,8 +1,11 @@
 package barath.springframework;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootConfiguration
 @SpringBootApplication
 public class PetCLinicApplication {
 
