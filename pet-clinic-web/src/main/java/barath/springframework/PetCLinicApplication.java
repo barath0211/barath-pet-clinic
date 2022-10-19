d8dcd4a74fe8544420c87d777fdcd98dbd153edf
barath0211 <barathkumaarabirami@gmail.com>
2022-10-19 12:38:09 +0530

    ERRORS

--- pet-clinic-web/src/main/java/barath/springframework/PetCLinicApplication.java
+++ pet-clinic-web/src/main/java/barath/springframework/PetCLinicApplication.java
@@ -3,7 +3,6 @@ package barath.springframework;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootConfiguration
 @SpringBootApplication
