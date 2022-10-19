83976316fb072d782892637c0ea2640929bbcb94
Karolina <200626@student.pwr.edu.pl>
2022-10-19 10:29:57 +0200

    fixed project

--- pet-clinic-web/src/main/java/barath/springframework/PetCLinicApplication.java
+++ pet-clinic-web/src/main/java/barath/springframework/PetCLinicApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootConfiguration
 @SpringBootApplication
 public class PetCLinicApplication {
 
