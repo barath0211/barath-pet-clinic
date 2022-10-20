53d0366295d610a1c153be38ba84b98bed1aad85
barath0211 <barathkumaarabirami@gmail.com>
2022-10-20 17:16:05 +0530

    Create Vet Page and Controller

---
+++ pet-clinic-web/src/main/java/barath/springframework/contollers/VetController.java
@@ -0,0 +1,14 @@
+package barath.springframework.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+
+    @RequestMapping({"/vets","/vets/index", "/vets/index.htmlf"})
+    public String listVEts(){
+        return "vets/index";
+    }
+}
