52b04450345473ba8d597477d32ca068268c4f6c
barath0211 <barathkumaarabirami@gmail.com>
2022-10-25 15:38:15 +0530

    List Owners

--- pet-clinic-web/src/main/java/barath/springframework/contollers/OwnerController.java
+++ pet-clinic-web/src/main/java/barath/springframework/contollers/OwnerController.java
@@ -1,14 +1,22 @@
 package barath.springframework.contollers;
 
+import barath.springframework.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"/owners","/owners/index", "/owners/index.htmlf"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
