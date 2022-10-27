f7890a32574ecce78f9091e0d6a8a511763a133a
barath0211 <barathkumaarabirami@gmail.com>
2022-10-27 11:25:08 +0530

    Created Vet Speciality Entity and Added to Vet

--- pet-clinic-data/src/main/java/barath/springframework/model/Vet.java
+++ pet-clinic-data/src/main/java/barath/springframework/model/Vet.java
@@ -1,4 +1,15 @@
 package barath.springframework.model;
 
+import java.io.Serializable;
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
