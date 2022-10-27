4d27ef407addd99dbee0a67ca8d8a1b5fbcaa17e
barath0211 <barathkumaarabirami@gmail.com>
2022-10-27 10:48:55 +0530

    Created Pet Type,Pet and Visit Entities

--- pet-clinic-data/src/main/java/barath/springframework/model/PetType.java
+++ pet-clinic-data/src/main/java/barath/springframework/model/PetType.java
@@ -1,4 +1,11 @@
 package barath.springframework.model;
 
 public class PetType extends BaseEntity{
+    private String name;
+    private String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
