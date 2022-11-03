034ebb81fc05acc9717af1d6d2c5afd0dd7fb683
barath0211 <barathkumaarabirami@gmail.com>
2022-11-03 11:34:12 +0530

    Converted Vets to JPA Entities

--- pet-clinic-data/src/main/java/barath/springframework/model/Speciality.java
+++ pet-clinic-data/src/main/java/barath/springframework/model/Speciality.java
@@ -1,9 +1,15 @@
 package barath.springframework.model;
 
+import org.hibernate.annotations.Cache;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends  BaseEntity{
-    public static Speciality savedRadiology;
-    public static Speciality savedSurgery;
-    public static Speciality savedDentistry;
+    @Column(name = "description")
     private  String description;
 
     public String getDescription() {
