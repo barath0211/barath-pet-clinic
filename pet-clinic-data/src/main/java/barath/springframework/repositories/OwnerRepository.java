76f8b168f2e4845ff1aa4616dd697fd99577e387
barath0211 <barathkumaarabirami@gmail.com>
2022-11-03 14:47:27 +0530

    Created Spring data JPA Service for Owners

--- pet-clinic-data/src/main/java/barath/springframework/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/barath/springframework/repositories/OwnerRepository.java
@@ -4,5 +4,6 @@ import barath.springframework.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastname);
 
 }
